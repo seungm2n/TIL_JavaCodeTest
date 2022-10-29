25dcaddac61c3c91fdf1f92d8ea639bd3ef94459
seungm2n <seungm2n2@gmail.com>
2022-10-29 19:11:10 +0900

    SQL/JAVA Repository 분할 및 Chap05 반복문 추가

---
+++ Chap02(변수)/src/kr/co/ezenac/variable/VariableTest.java
@@ -0,0 +1,18 @@
+package kr.co.ezenac.variable;
+
+public class VariableTest {
+
+	public static void main(String[] args) {
+		int age, count;
+		age = 10;
+		
+		int level = 10000;
+		System.out.println(age);
+		System.out.println(level);
+
+		System.out.println(age + level);
+		
+
+	}
+
+}
---
+++ Chap05(반복문)/src/kr/co/ezenac/loop/ForTest02.java
@@ -0,0 +1,26 @@
+package kr.co.ezenac.loop;
+
+//누적합계가 2000이상 일때 i의 값을 출력하시오.
+
+public class ForTest02 {
+
+	public static void main(String[] args) {
+		
+		int sum = 0;
+		int temp = 0;
+		
+		for (int i=1; i<=100; i++) {
+			sum += i;
+			if(sum >= 2000) {
+				temp = i;
+				break;			//감싸고 있는 제어문의 블록을 빠져 나오는 기능
+			}
+			//System.out.println("sum :" + sum);
+		}
+		
+		System.out.println("2000이상 일때 i의 값 : " + temp);
+		System.out.println("2000이상 일때 i의 값 : " + sum);
+
+	}
+
+}
