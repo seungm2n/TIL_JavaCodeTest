373ae5d76eb8350f8f0268985a2e3603db4bd088
seungm2n <seungm2n2@gmail.com>
2022-11-11 08:14:41 +0900

    유용한 클래스들 코드 테스트

---
+++ Chap14(유용한 클래스들)/src/kr/co/ezenac/object/MyDateTest.java
@@ -0,0 +1,44 @@
+package kr.co.ezenac.object;
+
+/*
+ * 	날짜를 구현한 클래스 MyDate가 있습니다.
+ * 	날짜가 같으면 equals() 메서드 결과가 true가 되도록 구현하시오.
+ * 
+ * 	hashCode() 메서드도 구현해 보시오.
+ */
+
+class MyDate {
+	
+}
+
+public class MyDateTest {
+
+	public static void main(String[] args) {
+		
+		//MyDate date1 = new MyDate(2, 9, 2022);
+		//MyDate date2 = new MyDate(2, 9, 2022);		
+		
+		//System.out.println(date1.equals(date2));
+		
+		
+	}
+
+	
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
