eda682853d175ff00f91b4dde671957e9638fe56
seungm2n <seungm2n2@gmail.com>
2022-11-22 09:18:40 +0900

    Chap20 자바IO 코드테스트

---
+++ Chap20(자바IO)/src/kr/co/ezenac/stream03/FileReaderTest.java
@@ -0,0 +1,21 @@
+package kr.co.ezenac.stream03;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileReaderTest {
+	
+	public static void main(String[] args) throws IOException {
+		
+		FileReader fr = new FileReader("reader.txt");
+		
+		int i;
+		while ( (i = fr.read()) != -1)
+			System.out.print((char)i);
+		
+		fr.close();
+		
+	}
+
+}
